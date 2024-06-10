7a3b093cf737deb6dc58a99b5f960cce38f42c0e
Matthew Baggett <matthew@baggett.me>
2024-06-10 06:06:37 +0200

    Add support for .env files

--- src/Services/EnvironmentService.php
+++ src/Services/EnvironmentService.php
@@ -11,7 +11,21 @@ class EnvironmentService
     public function __construct()
     {
         $this->environmentVariables = array_merge($_SERVER, $_ENV);
+        if(file_exists(APP_ROOT . '/.env')){
+            $env = file_get_contents(APP_ROOT . '/.env');
+            $lines = explode("\n", $env);
+            foreach($lines as $line){
+                $line = trim($line);
+                if($line == ''){
+                    continue;
+                }
+                $parts = explode('=', $line);
+                $this->environmentVariables[$parts[0]] = $parts[1];
+            }
+        }
+
         ksort($this->environmentVariables);
+
     }
 
     public function has(string $key): bool
